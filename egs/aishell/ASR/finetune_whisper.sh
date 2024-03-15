720d53b4b3a29e1f9c43e56fbe4ed625b041dde6
joey.wang <joey.wang@cloudminds.com>
2024-03-15 16:27:35 +0800

    egs/aishell finetune whisper: 20240315-2nd

--- egs/aishell/ASR/finetune_whisper.sh
+++ egs/aishell/ASR/finetune_whisper.sh
@@ -12,6 +12,7 @@ stage=1
 stop_stage=1
 
 num_nodes=1
+train_engine=torch_ddp
 
 # Automatically detect number and index of gpus
 if command -v nvidia-smi &> /dev/null; then
@@ -25,22 +26,28 @@ log "num_gpus: ${num_gpus}"
 
 if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
   log "Stage 1: fine-tuning on Aishell test set on whisper large-v2"
-  # fine-tuning with deepspeed
-  torchrun --nnodes=$num_nodes --nproc_per_node=$num_gpus \
-    ./whisper/train.py \
-      --max-duration 200 \
-      --num-epochs 10 \
-      --start-epoch 3 \
-      --exp-dir whisper/exp_large_v2 \
-      --model-name large-v2 \
-      --deepspeed \
-      --deepspeed_config ./whisper/ds_config_zero1.json
 
-  # fine-tuning with ddp
-#  torchrun --nproc_per_node 1 ./whisper/train.py \
-#    --max-duration 25 \
-#    --exp-dir whisper/exp_large_v2 \
-#    --model-name large-v2 \
-#    --manifest-dir data/fbank_whisper \
-#    --base-lr 1e-5
+  if [ ${train_engine} == "deepspeed" ]; then
+    log "using deepspeed"
+    # fine-tuning with deepspeed
+    torchrun --nnodes=$num_nodes --nproc_per_node=$num_gpus \
+      ./whisper/train.py \
+        --max-duration 200 \
+        --num-epochs 10 \
+        --start-epoch 3 \
+        --exp-dir whisper/exp_large_v2 \
+        --model-name large-v2 \
+        --deepspeed \
+        --deepspeed_config ./whisper/ds_config_zero1.json
+  else
+    log "using torch ddp"
+    # fine-tuning with ddp
+    torchrun --nnodes=$num_nodes --nproc_per_node=$num_gpus \
+        ./whisper/train.py \
+          --max-duration 200 \
+          --num-epochs 10 \
+          --start-epoch 3 \
+          --exp-dir whisper/exp_large_v2 \
+          --model-name large-v2
+  fi
 fi
