66583d15dbf1e08cf55174fe8f138585dbe91713
joey.wang <joey.wang@cloudminds.com>
2024-03-16 23:15:27 +0800

    egs/aishell finetune whisper: 20240315-3rd

--- egs/aishell/ASR/finetune_whisper.sh
+++ egs/aishell/ASR/finetune_whisper.sh
@@ -22,12 +22,23 @@ else
   num_gpus=-1
   gpu_list="-1"
 fi
+num_gpus=1
+gpu_list="0"
 log "num_gpus: ${num_gpus}"
 
 if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
   log "Stage 1: fine-tuning on Aishell test set on whisper large-v2"
-
-  if [ ${train_engine} == "deepspeed" ]; then
+  if [ ${train_engine} == "torch_ddp" ]; then
+    log "using torch_ddp"
+    # fine-tuning with ddp
+    torchrun --nnodes=$num_nodes --nproc_per_node=$num_gpus \
+        ./whisper/train.py \
+          --max-duration 50 \
+          --num-epochs 10 \
+          --start-epoch 3 \
+          --exp-dir whisper/exp_large_v2 \
+          --model-name large-v2
+  else
     log "using deepspeed"
     # fine-tuning with deepspeed
     torchrun --nnodes=$num_nodes --nproc_per_node=$num_gpus \
@@ -39,15 +50,5 @@ if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
         --model-name large-v2 \
         --deepspeed \
         --deepspeed_config ./whisper/ds_config_zero1.json
-  else
-    log "using torch ddp"
-    # fine-tuning with ddp
-    torchrun --nnodes=$num_nodes --nproc_per_node=$num_gpus \
-        ./whisper/train.py \
-          --max-duration 200 \
-          --num-epochs 10 \
-          --start-epoch 3 \
-          --exp-dir whisper/exp_large_v2 \
-          --model-name large-v2
   fi
 fi
